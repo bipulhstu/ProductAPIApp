e46c97c11d14bc975b7f314708223dd5120b4d14
Bipul Islam <bipulhstu@gmail.com>
2024-12-14 19:00:09 +0600

    Initial Commit

---
+++ ProductApp/ProductAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductAppApp.swift
+//  ProductApp
+//
+//  Created by Bipul Islam on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ProductAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
